bf693eb157dab8248cc16f2b1bf5b6ae504e4df4
eduit-learning <achavez@grupoeduit.com>
2023-09-06 11:25:21 -0600

    Ejercicico

--- src/app/contacto/contacto-routing.module.ts
+++ src/app/contacto/contacto-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearComponent } from './crear/crear.component';
+import { IndexComponent } from './index/index.component';
+import { ListarComponent } from './listar/listar.component';
+import { EditarComponent } from './editar/editar.component';
+import { EliminarComponent } from './eliminar/eliminar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contacto', redirectTo: 'contacto/index', pathMatch: 'full' },
+  { path: 'cc', redirectTo: 'contacto/crear', pathMatch: 'full' },
+  { path: 'c/c', redirectTo: 'contacto/crear', pathMatch: 'full' },
+  { path: 'contacto/index', component: IndexComponent },
+  { path: 'contacto/crear', component: CrearComponent },
+  { path: 'contacto/listar', component: ListarComponent },
+  { path: 'contacto/editar', component: EditarComponent },
+  { path: 'contacto/eliminar', component: EliminarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/contacto/contacto.module.ts
+++ src/app/contacto/contacto.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { ContactoRoutingModule } from './contacto-routing.module';
 import { IndexComponent } from './index/index.component';
 import { CrearComponent } from './crear/crear.component';
+import { ListarComponent } from './listar/listar.component';
+import { EliminarComponent } from './eliminar/eliminar.component';
+import { EditarComponent } from './editar/editar.component';
 
 
 @NgModule({
   declarations: [
     IndexComponent,
-    CrearComponent
+    CrearComponent,
+    ListarComponent,
+    EliminarComponent,
+    EditarComponent
   ],
   imports: [
     CommonModule,
