1618a52793c43599209e762febe30e944a730191
eduit-learning <achavez@grupoeduit.com>
2023-09-14 12:52:34 -0600

    ejercicio

--- src/app/contacto/editar/editar.component.ts
+++ src/app/contacto/editar/editar.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContactoService } from '../contacto.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Icontacto } from '../icontacto';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editar',
   templateUrl: './editar.component.html',
   styleUrls: ['./editar.component.css']
 })
-export class EditarComponent {
+export class EditarComponent implements OnInit {
+  requestID!: number;
+  cRequest!: Icontacto;
+  _form!: FormGroup;
+
+  constructor(
+    public contactoService: ContactoService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.requestID = this.route.snapshot.params['requestID']
+    console.log(this.requestID)
+
+    this._form = new FormGroup({
+      nombre: new FormControl('', [Validators.required,
+      Validators.maxLength(50), Validators.minLength(5)]),
+      email: new FormControl('', [Validators.required,
+      Validators.maxLength(50), Validators.minLength(5), Validators.email]),
+      mensaje: new FormControl('', [Validators.required,
+      Validators.maxLength(1000), Validators.minLength(5)]),
+      asunto: new FormControl('Sugerencia'),
+      contactar: new FormControl(true),
+      noticias: new FormControl(false),
+      prioridad: new FormControl(4)
+    });
+
+    this.contactoService.find(this.requestID).subscribe((data: Icontacto) => {
+      console.log(data);
+      this.cRequest = data;
+
+      this._form = new FormGroup({
+        nombre: new FormControl(this.cRequest.nombre, [Validators.required,
+        Validators.maxLength(50), Validators.minLength(5)]),
+        email: new FormControl(this.cRequest.email, [Validators.required,
+        Validators.maxLength(50), Validators.minLength(5), Validators.email]),
+        mensaje: new FormControl(this.cRequest.mensaje, [Validators.required,
+        Validators.maxLength(1000), Validators.minLength(5)]),
+        asunto: new FormControl(this.cRequest.asunto),
+        contactar: new FormControl(this.cRequest.contactar),
+        noticias: new FormControl(this.cRequest.noticias),
+        prioridad: new FormControl(this.cRequest.prioridad)
+      });
+    });
+    
+  }
+
+  get form() {
+    return this._form.controls;
+  }
+
+  submit() {
+    console.log(this._form.value);
+    /*this.contactoService.update(this._form.value).subscribe((res: any) => {
+      console.log('Elemento creado');
+      this.router.navigateByUrl('contacto/index');
+    });*/
+  }
 
 }
